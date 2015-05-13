08c709b7c7a2320720b62c35a816d2ab460607b0
TheBigvi <thebigvi1@gmail.com>
2015-05-13 11:31:53 +0200

    cleanup kodu + opis funkcji signal R

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -44,10 +44,79 @@ namespace WindowsFormsApplication1
             updateMapy();
             tb_Czat.Enabled = false;
             b_Wyslij.Enabled = false;
-            //NickPrzeciwnika = "dawid2";
         }
 
+        //========== funkcje związane z widocznymi obiektami (buttony, panel itp) ==========
+        private void Mouse_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            MouseEventArgs eM = (MouseEventArgs)e;
+            x = (eM.X - 43) / 34;
+            y = (eM.Y - 44) / 34;
 
+            if (eM.X > 40 && eM.Y > 40)
+            {
+                if (x >= 0 && x < 10 && y >= 0 && y < 10)
+                {
+                    lb_Log.Items.Add("Stzal na   x: " + (x + 1).ToString() + " y: " + (y + 1).ToString());
+                    lb_Log.SelectedIndex = lb_Log.Items.Count - 1;
+                    lb_Log.SelectedIndex = -1;
+                    HubProxy.Invoke("SendCoordinates", NickPrzeciwnika, y, x);
+                    PBPrzeciwnika[y, x].BackgroundImage = Image.FromFile("icons\\x.png");
+                }
+            }
+        }
+
+        private void b_Wyslij_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tb_Czat.Text))
+            {
+                HubProxy.Invoke("SendPrivateMessage", NickPrzeciwnika, tb_Czat.Text);
+                lb_Czat.Items.Add(UserName + ": " + tb_Czat.Text);
+            }
+            tb_Czat.Clear();
+        }
+
+        private void b_Polacz_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tb_NickGracza.Text))
+            {
+                UserName = tb_NickGracza.Text;
+                tb_NickGracza.Enabled = false;
+                lb_Log.Items.Add("Connecting to server...");
+                ConnectAsync();
+                b_Polacz.Enabled = false;
+                b_Polacz.Visible = false;
+                b_Graj.Enabled = true;
+                b_Graj.Visible = true;
+
+            }
+        }
+
+        private void b_Graj_Click(object sender, EventArgs e)
+        {
+            if (lb_ListaGraczy.SelectedIndex == -1)
+            {
+                MessageBox.Show("Wybierz przeciwnika!");
+            }
+            else
+            {
+                NickPrzeciwnika = lb_ListaGraczy.GetItemText(lb_ListaGraczy.Items[lb_ListaGraczy.SelectedIndex]);
+                lb_Log.Items.Add(NickPrzeciwnika);
+                if (String.Equals(NickPrzeciwnika, UserName))
+                {
+                    MessageBox.Show("Nie możesz grać sam ze sobą!!!");
+                }
+                else if (!String.Equals(NickPrzeciwnika, UserName))
+                {
+                    gramy();
+                }
+            }
+
+        }
+        //========== funkcje związane z widocznymi obiektami (buttony, panel itp) ==========
+
+        //========== funkcje moje ==========
         private void updateMapy()
         {
             string temp;
@@ -63,6 +132,28 @@ namespace WindowsFormsApplication1
             }
         }
 
+        private void gramy()
+        {
+            //========== logika ==========
+            th.Abort();
+            //========== logika ==========
+
+            //========== zmiana UI ==========
+            tb_NickGracza.Visible = false;
+            lb_ListaGraczy.Enabled = false;
+            lb_ListaGraczy.Visible = false;
+            label_Czat.Visible = true;
+            lb_Czat.Visible = true;
+            tb_Czat.Visible = true;
+            tb_Czat.Enabled = true;
+            b_Wyslij.Visible = true;
+            b_Wyslij.Enabled = true;
+            b_Graj.Enabled = false;
+            b_Graj.Visible = false;
+            TransparentPanel.Visible = true;
+            //========== zmiana UI ==========
+        }
+
        private void generowanie_grafiki()
         {
             int offsetX = 0;
@@ -70,8 +161,8 @@ namespace WindowsFormsApplication1
             for (int x = 0; x < 10; x++)
             {
                 if (x > 6)
-                    offsetX = 3;
-                for (int y = 0; y < 10; y++)
+                    offsetX = 3;                    //magia, nie czytaj
+                for (int y = 0; y < 10; y++)        //bo i tak nie zrozumiesz xD
                 {
                     if (y >= 3)
                         offsetY = 4;
@@ -107,93 +198,6 @@ namespace WindowsFormsApplication1
             LewaStrona.BackgroundImageLayout = ImageLayout.Stretch;
         }
 
-        private void b_Wyslij_Click(object sender, EventArgs e)
-        {
-            if(!String.IsNullOrEmpty(tb_Czat.Text))
-            {
-                HubProxy.Invoke("SendPrivateMessage", NickPrzeciwnika, tb_Czat.Text);
-            }
-            tb_Czat.Clear();       
-        }
-
-        private void Mouse_Click(object sender, EventArgs e)
-        {
-            int x, y;
-            MouseEventArgs eM = (MouseEventArgs)e;
-            x = (eM.X - 43) / 34;
-            y = (eM.Y - 44) / 34;
-
-            if(eM.X>40 && eM.Y>40)
-            {
-                if (x >= 0 && x < 10 && y >= 0 && y < 10)
-                {
-                    lb_Log.Items.Add("Stzal na   x: " + (x+1).ToString() + " y: " + (y+1).ToString());
-                    lb_Log.SelectedIndex = lb_Log.Items.Count - 1;
-                    lb_Log.SelectedIndex = -1;
-                    HubProxy.Invoke("SendCoordinates", NickPrzeciwnika, y, x);
-                    PBPrzeciwnika[y, x].BackgroundImage = Image.FromFile("icons\\x.png");
-                }
-            } 
-        }
-
-        private void b_Polacz_Click(object sender, EventArgs e)
-        {
-            if (!String.IsNullOrEmpty(tb_NickGracza.Text))
-            {
-                UserName = tb_NickGracza.Text;
-                tb_NickGracza.Enabled = false;
-                lb_Log.Items.Add("Connecting to server...");
-                ConnectAsync();
-                b_Polacz.Enabled = false;
-                b_Polacz.Visible = false;
-                b_Graj.Enabled = true;
-                b_Graj.Visible = true;
-              
-            }
-        }
-
-        private void b_Graj_Click(object sender, EventArgs e)
-        {
-            NickPrzeciwnika = lb_ListaGraczy.GetItemText(lb_ListaGraczy.Items[lb_ListaGraczy.SelectedIndex]);
-            //lb_Log.Items.Add(NickPrzeciwnika);
-
-            if(String.Equals(NickPrzeciwnika,"-1"))
-            {
-                MessageBox.Show("Wybierz przeciwnika!");
-            }
-            else if(String.Equals(NickPrzeciwnika, UserName))
-            {
-                MessageBox.Show("Nie możesz grać sam ze sobą!!!");
-            }
-            else if(!String.Equals(NickPrzeciwnika,UserName))
-            {
-                //========== logika ==========
-                th.Abort();
-                //========== logika ==========
-
-                //========== zmiana UI ==========
-                tb_NickGracza.Visible = false;
-                lb_ListaGraczy.Enabled = false;
-                lb_ListaGraczy.Visible = false;
-                label_Czat.Visible = true;
-                lb_Czat.Visible = true;
-                tb_Czat.Visible = true;
-                tb_Czat.Enabled = true;
-                b_Wyslij.Visible = true;
-                b_Wyslij.Enabled = true;
-                b_Graj.Enabled = false;
-                b_Graj.Visible = false;
-                TransparentPanel.Visible = true;
-                //========== zmiana UI ==========
-            }
-        }
-
-        //po 30s z automatu sie wylaczy
-        void Connection_Closed()
-        {
-
-        }
-
         //connect to server
         private async void ConnectAsync()
         {
@@ -207,34 +211,31 @@ namespace WindowsFormsApplication1
             }
             catch (HttpRequestException)
             {
-                //when error send communication to user
                 lb_Log.Items.Add("Unable to connect to server: Start server before connecting clients.");
                 return;
             }
 
-            lb_Log.Items.Add("Connected to server at " + ServerURI + "\r");
-
-            HubProxy.Invoke("AddUser", UserName);
+            lb_Log.Items.Add("Connected to server at " + ServerURI + "\r"); //połączenie z serwerem
+            HubProxy.Invoke("AddUser", UserName);                           //i dodanie siebie jako gracza
             
-            //HubProxy.Invoke("GetUsersList");
-            //lb_Log.Items.Add("po get user: "+UserName);
 
-            th = new Thread(odswiez);
-            th.IsBackground = true;
+            th = new Thread(odswiez);   //odpalamy watek ktory odswieza
+            th.IsBackground = true;     //liste graczy co 8 sekund
             th.Start();
 
-            HubProxy.On<int,int>("GetCoordinates", (y, x)=>
+            HubProxy.On<int,int>("GetCoordinates", (y, x)=>     //funkcja wykonuje sie po strzale przeciwnika
                 {
                     tb_Czat.Text = "x: " + x + " y: " + y;
                 });
-            HubProxy.On<List<UserDetail>>("SetUsersList", (ConnectedUsers2) =>
-                {
-                    //lb_Log.Items.Add("Proxy.ON SetUsersList");
+
+            HubProxy.On<List<UserDetail>>("SetUsersList", (ConnectedUsers2) =>      //po zazadaniu aktualizacji listy aktywnych
+                {                                                                   //userow serwer wywola na nas te funkcje
                     ConnectedUsers = ConnectedUsers2;
                     GetActiveUsers();
                 });
-            HubProxy.On<string, string>("addNewMessageToPage", (name, message) =>
-                 lb_Czat.Items.Add(name + ": " + message));
+
+            HubProxy.On<string, string>("addNewMessageToPage", (name, message) => //po napisaniu wiadomosci przez przeciwnika
+                 lb_Czat.Items.Add(name + ": " + message));                       //serwer wywola te funkcje na nas
         }
 
         private void GetActiveUsers()
@@ -247,11 +248,17 @@ namespace WindowsFormsApplication1
 
         private void odswiez()
         {
-            while (true)
-            {
+            while (true)                    //watek odswieza liste
+            {                               //graczy co 8 sekund
                 HubProxy.Invoke("GetUsersList");
                 System.Threading.Thread.Sleep(8000);
             }
+        }
+
+        //po 30s z automatu sie wylaczy
+        void Connection_Closed()
+        {
+            lb_Log.Items.Add("Halo halo mamy problem z połączeniem!");
         }
 
         private void WPFClient_Closing(object sender, System.ComponentModel.CancelEventArgs e)
